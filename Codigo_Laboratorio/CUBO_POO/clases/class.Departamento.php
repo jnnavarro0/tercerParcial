08446ee17add1f6c551649c82cb071a84a80189b
jnnavarro0 <jnnavarro@espe.edu.ec>
2023-02-19 00:18:03 -0500

    ,

--- Codigo_Laboratorio/CUBO_POO/clases/class.Departamento.php
+++ Codigo_Laboratorio/CUBO_POO/clases/class.Departamento.php
@@ -1,9 +1,101 @@
-<?php
-class Departamento extends Nivel{
-	private $Carrera;    // Matriz de datos
-    private $titulo;
-	
-    
+<!DOCTYPE html>
+<html lang="en">
 
-}
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+
+    use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
+    class Departamento extends Nivel
+    {
+        private $Carrera;    // Matriz de datos
+        private $titulo;
+
+        function __construct($Nom, $Num, $Carrera, $titulo)
+        {
+            parent::__construct($Nom, count($Num));
+            $this->Carrera = $Carrera;
+            $this->titulo = $titulo;
+        }
+
+        public function GetCarrera()
+        {
+            return $this->Carrera;
+        }
+
+        public function Gettitulo()
+        {
+            return $this->titulo;
+        }
+
+
+        public function CalcularMaxColum($cara)
+        {
+            $maxColum = null;
+            foreach ($cara as $prov => $arreglo) {
+                // $numProv = count($data[$prov]);
+                $tam = count($cara[$prov]);
+                $maxColum = ($maxColum >= $tam) ? $maxColum : $tam;
+            }
+
+            return $maxColum;
+        }
+
+
+        public function ImprimirNivel()
+        {
+            $niveles = [];
+            $cont = 0;
+
+            echo '<br><h1 class="text-center">Carrera Ingeniería ' . $this->GetCarrera() . '</h1><br>';
+
+            foreach ($this->Gettitulo() as $indice => $materias) {
+                $niveles[$cont] = $indice;
+                $cont++;
+            }
+
+            $cont = 0;
+
+            foreach ($this->Gettitulo() as $indice => $materias) {
+                if ($indice == $niveles[$cont]) {
+
+                    $table = '<div class="container"><center><table class="table table-bordered table-striped"><tr><td colspan=' . 
+                    count($materias) .' class="table-success"><center><b>' . $indice . ' de ' . $this->GetCarrera() . 
+                    '</b></center></td></tr><tr>';
+
+                    foreach ($materias as $carrera => $materia)
+                        $table .= '<th class="table-secondary">' . $carrera . '</th>';
+
+                    $table .= '</tr>';
+
+                    for ($i = 0; $i < $this->CalcularMaxColum($materias); $i++) {
+
+                        $table .= '<tr>';
+
+                        foreach ($materias as $carrera => $materia)
+                            $table .= (isset($materia[$i])) ? '<td>' . $materia[$i] . '</td>' : '<td></td>';
+
+                        $table .= '</tr>';
+                    }
+
+                    $table .= '</table></center></div>';
+                }
+
+                echo '<br>' . $table . '<br><br>';
+                $cont++;
+            }
+        }
+    }
+    ?>
+
+</body>
+
+</html>
--- Codigo_Laboratorio/CUBO_POO/index.php
+++ Codigo_Laboratorio/CUBO_POO/index.php
@@ -3,22 +3,20 @@ include_once("cubo.php");
 require_once("clases/class.Nivel.php");	
 require_once("clases/class.Departamento.php");
 
+echo "<a href='../index.html'>Regresar</a>";
+
 imprimirPoo();
 
 
 //REGRESAR
-echo "<a href='../index.html'>Regresar</a>";
+
 
 
 function imprimirPoo(){
-	global $sistemas,$mecanica,$electronica;
+	global $sistemas,$mecanica,$electronica, $departamento;
 	$titulo = ["SISTEMAS","ELECTRONICA","MECANICA"];
-	$objSIS = new Departamento($titulo[0],$sistemas);
-    $objELE = new Departamento($titulo[1],$electronica);
-	$objMEC = new Departamento($titulo[2],$mecanica);
-    $objSIS->ImprimirNivel();
+	$objSIS = new Departamento('',$departamento['Computacion'], $titulo[0],$sistemas);
+    $objELE = new Departamento('',$departamento['Electrica y electronica'], $titulo[1],$electronica);
+	$objMEC = new Departamento('',$departamento['Mecanica'], $titulo[2],$mecanica);
 	$objELE->ImprimirNivel();
-	$objMEC->ImprimirNivel();
 }
-
-?>
